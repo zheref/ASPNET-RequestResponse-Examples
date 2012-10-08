c551a744f368436666b9da27b785a16ec6907fde
Sergio Daniel Lozano García <zheref@live.com>
2012-10-08 09:57:19 -0500

    ASP.NET Way First Part

---
+++ Razor-HTML-CSS/funcs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+public static class Functions
+{
+    public static void Load(string name)
+    {
+        return WebPage.RenderPage(name);
+    }
+}
