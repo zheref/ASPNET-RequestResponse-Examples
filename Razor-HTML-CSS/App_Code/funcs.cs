7dcc373a8119b0b6278f6dc13bb4494f8804d05e
Sergio Daniel Lozano García <zheref@live.com>
2012-10-09 05:58:36 -0500

    Segunda Parte (completo)
    Creada seccion de App_Code para codigo compilado
    Manejo de sesion establecida

---
+++ Razor-HTML-CSS/App_Code/funcs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+public static class AuthData
+{
+    public const string USERNAME = "zheref";
+    public const string PASSWORD = "Altairis";
+
+    public static bool IsLogged(HttpSessionStateBase session)
+    {
+        return session["username"] != null;
+    }
+}
